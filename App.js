7fde7d21cd257b36e33aa4aa835794b31b92f2e2
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-12-01 10:01:28 +0100

    setup med firebase

--- App.js
+++ App.js
@@ -2,6 +2,28 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+
+// FIREBASE SDK CONFIG
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDp9nPrjUjhVwrUYAUYg3d6B8UxFGy5TIk",
+  authDomain: "spotter-8eaff.firebaseapp.com",
+  projectId: "spotter-8eaff",
+  storageBucket: "spotter-8eaff.appspot.com",
+  messagingSenderId: "1085645488811",
+  appId: "1:1085645488811:web:623f069be421c1b2b3adee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
 export default function App() {
   return (
     <View style={styles.container}>
