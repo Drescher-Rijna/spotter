0ae0989c26f7ff29dd240a217c83ca74a65e0144
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-12-03 13:23:13 +0100

    form set up
    mangler gps lokation, mulighed for at indskrive adresse og en submit knap

--- screens/CreateStepOne.js
+++ screens/CreateStepOne.js
@@ -1,14 +1,52 @@
 import { Formik } from 'formik'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
-
+import * as Location from 'expo-location';
 
 export default function CreateStepOne() {
+    const [location, setLocation] = useState(null);
+    const [address, setAddress] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [maps, setMaps] = useState(null);
+
+    useEffect(async () => {
+            //PERMISSION REQUEST
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+            return;
+            }
+
+            // GETTING LOCATION IF GRANTED
+            let location = await Location.getCurrentPositionAsync({});
+            
+            // USING COORDINATES TO GET ADDRESS DATA
+            let response = await Location.reverseGeocodeAsync({ latitude: location.coords.latitude, longitude: location.coords.longitude });
+            setLocation(response);
+
+            // LOOP THROUGH ADRESS DATA TO WRITE IT OUT IN SPECIFIC WAY
+            for (let item of response) {
+                let resultAddress = `${item.street} ${item.name}, ${item.postalCode} ${item.city}`;
+                
+                // SET THE ADDRESS TO A STATE
+                setAddress(resultAddress);
+            }
+        
+    }, []);
+
+    // USING STATE ON VARIABLE WITH IF CHECK
+    let userLocation = 'Waiting..';
+    if (errorMsg) {
+        userLocation = errorMsg;
+    } else if (address) {
+        userLocation = address;
+    }
+
     return (
         <View>
             <Formik
-                initialValues={{ title: '', location: '', category: '', body: '',  }}
+                initialValues={{ title: '', location: userLocation, category: '', body: '',  }}
                 onSubmit={(values) => {
 
                 }}
