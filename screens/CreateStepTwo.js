bd6f0bc7c0d6e2a3a29bc62f50d847bbffd6c5b2
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-12-06 13:36:47 +0100

    done
    everything is done. But there is a problem with the form. Need to try without Formik

--- screens/CreateStepTwo.js
+++ screens/CreateStepTwo.js
@@ -11,6 +11,7 @@ export default function CreateStepTwo(props) {
     const [location, setLocation] = useState(null);
     const [address, setAddress] = useState(null);
     const [errorMsg, setErrorMsg] = useState(null);
+    const [coordinates, setCoordinates] = useState({latitude: 0, longitude: 0});
 
     // Modal state
     const [modalOpen, setModalOpen] = useState(false);
@@ -25,10 +26,14 @@ export default function CreateStepTwo(props) {
 
             // GETTING LOCATION IF GRANTED
             let location = await Location.getCurrentPositionAsync({});
+            console.log('coords');
+            console.log(location.coords.longitudeDelta);
+            
             
             // USING COORDINATES TO GET ADDRESS DATA
             let response = await Location.reverseGeocodeAsync({ latitude: location.coords.latitude, longitude: location.coords.longitude });
             setLocation(response);
+            
 
             // LOOP THROUGH ADRESS DATA TO WRITE IT OUT IN SPECIFIC WAY
             for (let item of response) {
@@ -37,16 +42,31 @@ export default function CreateStepTwo(props) {
                 // SET THE ADDRESS TO A STATE
                 setAddress(resultAddress);
             }
+
+            setCoordinates({latitude: location.coords.latitude, longitude: location.coords.longitude});
+            console.log('2');
+            console.log(coordinates);
         
     }, []);
 
-    // USING STATE ON VARIABLE WITH IF CHECK
     let userLocation = 'Waiting..';
     if (errorMsg) {
         userLocation = errorMsg;
     } else if (address) {
         userLocation = address;
     }
+    useEffect(() => {
+        
+        if (errorMsg) {
+            userLocation = errorMsg;
+        } else if (address) {
+            userLocation = address;
+            console.log(userLocation);
+        }
+    }, [address])
+
+    // USING STATE ON VARIABLE WITH IF CHECK
+    
 
     return (
         <View style={styles.container}>
@@ -135,38 +155,63 @@ export default function CreateStepTwo(props) {
             {/* Modal for typing in a new location other than the users current location */}
             <Modal visible={modalOpen} >
                 <View style={{flex: 1}}>
-                    <GooglePlacesAutocomplete 
-                        placeholder="Search..."
+                    <GooglePlacesAutocomplete
+                        placeholder="Search"
                         fetchDetails={true}
                         GooglePlacesSearchQuery={{
                             rankby: "distance"
                         }}
+                        onFail={(error) => console.error(error)}
                         onPress={(data, details = null) => {
-                            // change coords state to new coords
-                            console.log(data, details);
+                            // 'details' is provided when fetchDetails = true
+                            setCoordinates({
+                                latitude: details.geometry.location.lat,
+                                longitude: details.geometry.location.lng,
+                            })
                         }}
                         query={{
-                            key: 'AIzaSyDp9nPrjUjhVwrUYAUYg3d6B8UxFGy5TIk',
-                            language: 'da',
+                            key: "AIzaSyC98flPHNxCKCi2Sq3oxKJ4kVdeApkwR3c",
+                            language: "da",
                             components: "country:dk",
-                            types: "establishment",
-                            radius: 30000,
                             location: userLocation
                         }}
-
-                        style={{ flex: 1, position: "absolute", width: "100%", zIndex: 1}}
+                        styles={{
+                            container: { flex: 0, position: "absolute", width: "100%", zIndex: 5, borderRadius: 0 },
+                            listView: { backgroundColor: "white", zIndex: 5 }
+                        }}
                     />
 
-                    <View style={{flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 10}}>
+                    <View style={{zIndex: 3, flexDirection: 'row', justifyContent: 'space-between', marginHorizontal: 10, marginTop: 55, marginBottom: 10}}>
                         <Button title="Back" onPress={() => setModalOpen(!modalOpen)} />
                         {/* Make it so when they press the new coords are made to a address and make choosenLocation into that */}
-                        <Button title="Confirm location" onPress={() => setModalOpen(!modalOpen)} />
+                        <Button title="Confirm location" onPress={async() => {
+                                let result = await Location.reverseGeocodeAsync({latitude: coordinates.latitude, longitude: coordinates.longitude});
+                                setAddress(result);
+                                setModalOpen(!modalOpen);
+                            }} 
+                        />
                     </View>
 
-                    <MapView style={styles.map} initialRegion={userLocation}>
-                        <Marker coordinate={userLocation} />
+                    <MapView
+                        style={styles.map}
+                        initialRegion={{
+                            longitude: coordinates.longitude, 
+                            latitude: coordinates.latitude, 
+                            longitudeDelta: 0.0421, 
+                            latitudeDelta: 0.0922
+                        }}
+                        region={{
+                            longitude: coordinates.longitude, 
+                            latitude: coordinates.latitude, 
+                            longitudeDelta: 0.0121, 
+                            latitudeDelta: 0.0122
+                        }}
+                        provider="google"
+                    >
+                        <Marker coordinate={coordinates} />
                     </MapView>
-                </View>
+		        </View>
+                
             </Modal>
         </View>
     )
@@ -230,7 +275,7 @@ const styles = StyleSheet.create({
     },
     map: {
         width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height
+        height: Dimensions.get("window").height,
     }
 })
 
