49641d9bc922b8eb1db4e7ab3ddf67e40bcdeb5b
Drescher-Rijna <82637513+Drescher-Rijna@users.noreply.github.com>
2021-12-07 14:46:30 +0100

    components for display

---
+++ components/SpotDetails.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SpotDetails = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default SpotDetails
+
+const styles = StyleSheet.create({})
+
---
+++ components/SpotPreview.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const SpotPreview = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default SpotPreview
+
+const styles = StyleSheet.create({})
+
